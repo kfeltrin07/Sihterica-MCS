17b637bd39677546967455893077097fc35c35d5
KARLO-PC\Karlo <karlo.feltrin@mcs-informatika.hr>
2024-12-19 09:19:08 +0100

    build projekta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  public translationLoaded: boolean = true;
+
+
+  ngOnInit(): void {
+    
+  }
   title = 'sihterica-projekt';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainContentComponent } from './components/base-layout/main-content/main-content.component';
+import { SidenavComponent } from './components/base-layout/sidenav/sidenav.component';
+import { ToolbarComponent } from './components/base-layout/toolbar/toolbar.component';
+import { LoadingComponent } from './components/elements/loading/loading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContentComponent,
+    SidenavComponent,
+    ToolbarComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
